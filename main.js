e7f514ec045a6b9adf3f3975302f5fe93063e6d0
FatHexy <fatkhul.abd3@gmail.com>
2023-09-08 14:51:47 +0700

    update encode

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
-let display = document.getElementById('output');
-let expression = '';
+// let display = document.getElementById('output');
+// let expression = '';
 
-function appendValue(value) {
-  expression += value;
-  display.value = expression;
+// function appendValue(value) {
+//   expression += value;
+//   display.value = expression;
+// }
+
+// function clearValue() {
+//   // slice(-1) menghapus setiap value terakhir yang ada dalam ekspression
+//   expression = expression.slice(0, -1);
+//   display.value = expression;
+// }
+
+// function clearDisplay() {
+//   expression = '';
+//   display.value = '';
+// }
+
+// function calculate() {
+//   try {
+//     let result = eval(expression);
+//     display.value = result;
+//     expression = '';
+//   } catch (error) {
+//     display.value = 'Gabisa Blok🥸';
+//   }
+// }
+
+// Encode JS
+var _0x1631 = [
+  "\x6F\x75\x74\x70\x75\x74",
+  "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64",
+  "",
+  "\x76\x61\x6C\x75\x65",
+  "\x73\x6C\x69\x63\x65",
+  "\x47\x61\x62\x69\x73\x61\x20\x42\x6C\x6F\x6B\uD83E\uDD78",
+];
+let display = document[_0x1631[1]](_0x1631[0]);
+let expression = _0x1631[2];
+function appendValue(_0xf235x4) {
+  expression += _0xf235x4;
+  display[_0x1631[3]] = expression;
 }
-
 function clearValue() {
-  // slice(-1) menghapus setiap value terakhir yang ada dalam ekspression
-  expression = expression.slice(0, -1);
-  display.value = expression;
+  expression = expression[_0x1631[4]](0, -1);
+  display[_0x1631[3]] = expression;
 }
-
 function clearDisplay() {
-  expression = '';
-  display.value = '';
+  expression = _0x1631[2];
+  display[_0x1631[3]] = _0x1631[2];
 }
-
 function calculate() {
   try {
-    let result = eval(expression);
-    display.value = result;
-    expression = '';
+    let _0xf235x8 = eval(expression);
+    display[_0x1631[3]] = _0xf235x8;
+    expression = _0x1631[2];
   } catch (error) {
-    display.value = 'Gabisa Blok🥸';
+    display[_0x1631[3]] = _0x1631[5];
   }
 }
